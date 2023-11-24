01e18a3c0090ad72820d6719d3aba1c7aa0d6aaf
MatubaraTooru <106291581+MatubaraTooru@users.noreply.github.com>
2023-11-24 18:39:11 +0900

    [update] Task

--- Assets/Datas/2023_11_23/CubeRotate.cs
+++ Assets/Datas/2023_11_23/CubeRotate.cs
@@ -1,30 +1,31 @@
 using System.Threading;
 using UnityEngine;
 using UnityEditor;
+using System.Threading.Tasks;
 
 public class CubeRotate : MonoBehaviour
 {
-    private CancellationTokenSource _cts = new CancellationTokenSource();
     private void Start()
     {
-        var ct = _cts.Token;
-        for (int i = 0; i < 7; i++)
+        // スレッドの最適化をアプリケーションが行うのは困難。
+        // ハードウェア環境などにより最適なスレッド数が異なる。
+
+        // そこで、一般的に C# では Thread の代わりに Task を使う。
+        // Task は実行したい処理から空いているスレッドを割り当てる管理を最適化してくれる。
+        Debug.Log($"Start: {Thread.CurrentThread.ManagedThreadId}");
+        for (var i = 0; i < 100; i++)
         {
-            var thread = new Thread(() =>
+            var taskID = i;
+            Task.Run(() =>
             {
-                while (!ct.IsCancellationRequested) { }
+                // 毎回新しいスレッドを作るのではなく、空いているスレッドを再利用するはず。
+                Debug.Log($"Task{taskID}: {Thread.CurrentThread.ManagedThreadId}");
             });
-            thread.Start();
         }
     }
     private void Update()
     {
         transform.Rotate(0, 1, 0);
-        for (var i = 0; i < 1000000000; i++) { }
-    }
-    private void OnDestroy()
-    {
-        _cts.Cancel();
     }
 }
 
